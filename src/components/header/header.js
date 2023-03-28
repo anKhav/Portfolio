c6bdfacf3dbf202cba1d300090bf1878316946e9
anKhav <khavaldzhi.a@gmail.com>
2023-03-28 16:31:06 +0200

    Added burger menu for mobile screen-endpoint.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "gatsby";
 import Logo from "../logo/logo";
-import {header, nav, link, container} from './header.module.scss'
+import {header, nav, link, container,toggler, toggler__content, open} from './header.module.scss'
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    console.log(isOpen);
     return (
         <header className={header}>
             <div className={container}>
                 <Logo/>
-                <nav className={nav}>
+                <button className={toggler} onClick={e => {
+                        e.preventDefault()
+                        setIsOpen(prevState => !prevState)
+                    }
+                }>
+                    <span className={toggler__content}></span>
+                </button>
+                <nav className={isOpen ? nav + ' ' + open : nav}>
                     <Link className={link} to="/">Home</Link>
                     <Link className={link} to="/about">About</Link>
                     <Link className={link} to="/projects">Projects</Link>
